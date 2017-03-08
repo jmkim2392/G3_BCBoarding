bd85561743a40edefb0497cc2457bef44f293a26
jmkim2392 <jmkim2392@gmail.com>
2017-03-08 12:31:56 -0800

    fixed subnav javascript

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     $(".accordionHeader").click(function () {
-        $(this).next().slideToggle(500, "swing");
+        if ($(this).next().is(":hidden")) {
+            $(".accordionContent").hide(500, "swing")
+            $(this).next().show(500, "swing", function () {
+                $('html, body').animate({
+                    scrollTop: $(this).offset().top - 165
+                }, 1000);
+            });
+        }
+        else {
+            $(".accordionContent").hide(500, "swing")
+            }
     });
 });
