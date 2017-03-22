b9b9a1a40fed0e7914a67e80b6f59c0d1fcf4120
Shiv <shiv_604@hotmail.com>
2017-03-21 18:28:27 -0700

    added furthur javascript validation

--- script.js
+++ script.js
@@ -10,62 +10,66 @@ $(document).ready(function () {
         }
         else {
             $(".accordionContent").hide(500, "swing")
-            }
+        }
     });
 });
 
 function validatecname() {
+    var regexp1 = /^[a-z0-9-_\.]+$/i;
     var errid = document.getElementById("errcname");
     var id = document.getElementById("username");
-    if (id.value === "") {
-        errid.setAttribute("style", "visibility:visible");
+    if (regexp1.test(document.getElementById("username").value)) {
+        errid.setAttribute("style", "visibility:hidden");
     }
     else {
-        errid.setAttribute("style", "visibility:hidden");
+        errid.setAttribute("style", "visibility:visible");
     }
 }
 
 function validatecpass() {
+    var regexp1 = /^[a-z0-9]+$/i;
     var errid = document.getElementById("errcpass");
     var id = document.getElementById("cpassword");
-    if (id.value === "") {
-        errid.setAttribute("style", "visibility:visible");
+    if (regexp1.test(document.getElementById("cpassword").value)) {
+        errid.setAttribute("style", "visibility:hidden");
     }
     else {
-        errid.setAttribute("style", "visibility:hidden");
+        errid.setAttribute("style", "visibility:visible");
     }
 }
 
 function validateemail() {
     var errid = document.getElementById("erremail");
-    var regexp1 = /[^A-Z0-9._-]/;
+    var regexp1 = /\S+@\S+\.\S+/;
     if (regexp1.test(document.getElementById("email").value)) {
-        errid.setAttribute("style", "visibility:visible");
-        return false;
+        errid.setAttribute("style", "visibility:hidden");
+        return true;
     }
     else {
-        errid.setAttribute("style", "visibility:hidden");
+        errid.setAttribute("style", "visibility:visible");
     }
 }
 
 function validatename() {
+    var regexp1 = /^[a-z0-9-_\.]+$/i;
     var errid = document.getElementById("errname");
     var id = document.getElementById("name");
-    if (id.value === "") {
-        errid.setAttribute("style", "visibility:visible");
+    if (regexp1.test(document.getElementById("name").value)) {
+        errid.setAttribute("style", "visibility:hidden");
     }
     else {
-        errid.setAttribute("style", "visibility:hidden");
+        errid.setAttribute("style", "visibility:visible");
     }
 }
 
 function validatepass() {
+    var regexp1 = /^[a-z0-9]+$/i;
     var errid = document.getElementById("errpass");
     var id = document.getElementById("pass");
-    if (id.value === "") {
-        errid.setAttribute("style", "visibility:visible");
+    if (regexp1.test(document.getElementById("pass").value)) {
+        errid.setAttribute("style", "visibility:hidden");
     }
     else {
-        errid.setAttribute("style", "visibility:hidden");
+        errid.setAttribute("style", "visibility:visible");
     }
 }
