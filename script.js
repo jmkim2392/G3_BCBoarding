5c950bade911169d054717a2b5bd04f94e6f1217
Jason Kim <jjkim2320@gmail.com>
2017-04-09 18:13:05 -0700

    changes jason

--- script.js
+++ script.js
@@ -15,39 +15,248 @@
      });
  });
  /* Slopes jquery */
- /*$(document).ready(function () {
+ /*change hover to click and make close button*/
+$(document).ready(function () {
      $(".SlopesCard").hover(function () {
          $(this).find(".SlopesCardImg").fadeTo(300, 0.2);
-         $(this).find(".SlopesPreview").fadeTo(300, 1);
      }, function () {
          $(this).find(".SlopesCardImg").fadeTo(100, 1);
-         $(this).find(".SlopesPreview").fadeTo(100, 0);
      });
  });
-*/
-/*change hover to click and make close button*/
 $(document).ready(function () {
-     $(".BigWhite").hover(function () {
-         $(this).find(".SlopesCardImg").fadeTo(300, 0.2);
+     $(".BigWhite").click(function () {
          $(".BigWhiteContent").fadeTo(300, 1);
-     }, function () {
-         $(this).find(".SlopesCardImg").fadeTo(100, 1);
-         $(".BigWhiteContent").fadeTo(100, 0);
+         $(".BigWhiteContent").css({
+             "z-index": "1"
+         });
+         bigWhiteMap();
      });
  });
-
 $(document).ready(function () {
-     $(".Cypress").hover(function () {
-         $(this).find(".SlopesCardImg").fadeTo(300, 0.2);
+     $(".Cypress").click(function () {
          $(".CypressContent").fadeTo(300, 1);
-     }, function () {
-         $(this).find(".SlopesCardImg").fadeTo(100, 1);
-         $(".CypressContent").fadeTo(100, 0);
+         $(".CypressContent").css({
+             "z-index": "1"
+         });
+         cypressMap();
+     });
+ });
+$(document).ready(function () {
+     $(".Grouse").click(function () {
+         $(".grouseContent").fadeTo(300, 1);
+         $(".grouseContent").css({
+             "z-index": "1"
+         });
+         grouseMap();
+     });
+ });
+$(document).ready(function () {
+     $(".Seymour").click(function () {
+         $(".seymourContent").fadeTo(300, 1);
+         $(".seymourContent").css({
+             "z-index": "1"
+         });
+         seymourMap();
+     });
+ });
+$(document).ready(function () {
+     $(".SunPeaks").click(function () {
+         $(".sunPeaksContent").fadeTo(300, 1);
+         $(".sunPeaksContent").css({
+             "z-index": "1"
+         });
+         sunPeaksMap();
+     });
+ });
+$(document).ready(function () {
+     $(".Whistler").click(function () {
+         $(".whistlerContent").fadeTo(300, 1);
+         $(".whistlerContent").css({
+             "z-index": "1"
+         });
+         whistlerMap();
      });
  });
 
+ /* Google Map */
+ function bigWhiteMap() {
+     var centermap = {
+         lat: 49.7355
+         , lng: -118.9414
+     };
+     var bigWhite = {
+         lat: 49.7216
+         , lng: -118.9266
+     };
+     var map = new google.maps.Map(document.getElementById('bigWhiteMap'), {
+         zoom: 12
+         , center: centermap
+         , panControl: true
+         , zoomControl: true
+         , mapTypeControl: true
+         , scaleControl: true
+         , streetViewControl: true
+         , overviewMapControl: true
+         , rotateControl: true
+     });
+     var marker = new google.maps.Marker({
+         position: bigWhite
+         , map: map
+     });
+ }
+function cypressMap() {
+     var centermap = {
+       lat: 49.3959
+         , lng: -123.2046
+     };
+     var cypress = {
+         lat: 49.3959
+         , lng: -123.2046
+     };
+     var map = new google.maps.Map(document.getElementById('cypressMap'), {
+         zoom: 14
+         , center: centermap
+         , panControl: true
+         , zoomControl: true
+         , mapTypeControl: true
+         , scaleControl: true
+         , streetViewControl: true
+         , overviewMapControl: true
+         , rotateControl: true
+     });
+     var marker = new google.maps.Marker({
+         position: cypress
+         , map: map
+     });
+ }
+function grouseMap() {
+     var centermap = {
+         lat: 49.3722
+         , lng: -123.0996
+     };
+     var grouse = {
+         lat: 49.3722
+         , lng: -123.0996
+     };
+     var map = new google.maps.Map(document.getElementById('grouseMap'), {
+         zoom: 13
+         , center: centermap
+         , panControl: true
+         , zoomControl: true
+         , mapTypeControl: true
+         , scaleControl: true
+         , streetViewControl: true
+         , overviewMapControl: true
+         , rotateControl: true
+     });
+     var marker = new google.maps.Marker({
+         position: grouse
+         , map: map
+     });
+ }
+function seymourMap() {
+     var centermap = {
+      lat: 49.3654
+         , lng: -122.9483
+     };
+     var seymour = {
+         lat: 49.3654
+         , lng: -122.9483
+     };
+     var map = new google.maps.Map(document.getElementById('seymourMap'), {
+         zoom: 15
+         , center: centermap
+         , panControl: true
+         , zoomControl: true
+         , mapTypeControl: true
+         , scaleControl: true
+         , streetViewControl: true
+         , overviewMapControl: true
+         , rotateControl: true
+     });
+     var marker = new google.maps.Marker({
+         position: seymour
+         , map: map
+     });
+ }
+function sunPeaksMap() {
+     var centermap = {
+  lat: 50.8844
+         , lng: -119.8859
+     };
+     var sunPeaks = {
+         lat: 50.8844
+         , lng: -119.8859
+     };
+     var map = new google.maps.Map(document.getElementById('sunPeaksMap'), {
+         zoom: 12
+         , center: centermap
+         , panControl: true
+         , zoomControl: true
+         , mapTypeControl: true
+         , scaleControl: true
+         , streetViewControl: true
+         , overviewMapControl: true
+         , rotateControl: true
+     });
+     var marker = new google.maps.Marker({
+         position: sunPeaks
+         , map: map
+     });
+ }
+function whistlerMap() {
+     var centermap = {
+       lat: 50.1162
+         , lng: -122.9574
+     };
+     var whistler = {
+         lat: 50.1162
+         , lng: -122.9574
+     };
+     var map = new google.maps.Map(document.getElementById('whistlerMap'), {
+         zoom: 12
+         , center: centermap
+         , panControl: true
+         , zoomControl: true
+         , mapTypeControl: true
+         , scaleControl: true
+         , streetViewControl: true
+         , overviewMapControl: true
+         , rotateControl: true
+     });
+     var marker = new google.maps.Marker({
+         position: whistler
+         , map: map
+     });
+ }
+ /* Back to Top button */
+ window.onscroll = function () {
+     scrollFunction()
+ };
 
+ /* Close button */
+ $(document).ready(function () {
+     $(".closeBtn").click(function () {
+         $(this).parent().css({
+             "z-index": "0"
+         });
+         $(this).parent().hide();
+     });
+ });
 
+ function scrollFunction() {
+     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+         document.getElementById("backToTop").style.display = "block";
+     }
+     else {
+         document.getElementById("backToTop").style.display = "none";
+     }
+ }
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+     document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0;
+ }
  $(document).ready(function () {
      $(function () {
          $("#date").datepicker();
