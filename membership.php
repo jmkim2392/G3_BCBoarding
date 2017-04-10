762de4c2ca630486d29168b27851550a8e79ccaa
Jason Kim <jjkim2320@gmail.com>
2017-04-09 23:51:03 -0700

    php

--- membership.php
+++ membership.php
@@ -1,6 +1,14 @@
 <?php
+	include 'functions.php';
+	require_once('config.php');
 	session_start();
+
+	// Connect to server and select database.
+	($GLOBALS["___mysqli_ston"] = mysqli_connect(DB_HOST,  DB_USER,  DB_PASSWORD))or die("cannot connect, error: ".((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+	((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . constant('DB_DATABASE')))or die("cannot select DB, error: ".((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+	$tbl_name="topic"; // Table name
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
