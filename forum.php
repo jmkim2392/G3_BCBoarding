dbb7f0ee73960a5b42a5463b08aeeeea6c3f26ba
Jason Kim <jjkim2320@gmail.com>
2017-04-10 21:38:57 -0700

    edited title

--- forum.php
+++ forum.php
@@ -19,7 +19,7 @@ session_start();
 
     
    <head>
-    <title>BCBoarding</title>
+    <title>PowderLine</title>
      <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="Style/base.css">
